30f2dccfb3bcd79561ab6035c1ec75b1e8bd4e7f
Scott Gellock <scott.gellock@microsoft.com>
2019-03-21 12:55:31 -0700

    Update CoreBot, EchoBot, and EmptyBot with 4.3 code
    - update using ActivityHandler and MVC pattern
    - update csproj files to use 4.3.2 bot framework assemblies
    - update README.md and PREREQUISITES.md files where needed

--- generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/EchoBot/Controllers/BotController.cs
+++ generators/vsix-vs-win/BotBuilderVSIX-V4/UncompressedProjectTemplates/EchoBot/Controllers/BotController.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System.Threading.Tasks;
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 
-namespace Microsoft.BotFramework.EmptyBot
+namespace $safeprojectname$
 {
     // This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
     // implementation at runtime. Multiple different IBot implementations running at different endpoints can be
